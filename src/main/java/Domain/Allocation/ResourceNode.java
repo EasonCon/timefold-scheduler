ed95735da2703dc1b577c21f50da5c69318ef97d
XinlongCONG <congxinlong@mail.teletraan.io>
2024-10-24 16:29:12 +0800

    feat:get resource end

--- src/main/java/Domain/Allocation/ResourceNode.java
+++ src/main/java/Domain/Allocation/ResourceNode.java
@@ -20,11 +20,24 @@ public class ResourceNode extends AllocationOrResource {
     }
 
     public Long getEndTime(Long startTime, long duration) {
-        if (startTime == null) {
+        if (startTime == null || startTime >= this.getTimeSlots().getLast().getEnd()) {
             return null;
         }
-        for(TimeSlot timeSlot : timeSlots) {
-
+        long remainingDuration = duration;
+        long endTime = this.getTimeSlots().getLast().getEnd();  // No reassignment, indicating that it has reached the rightmost
+        for (TimeSlot timeSlot : timeSlots) {
+            if (timeSlot.getEnd() <= startTime) {
+                continue;
+            }
+            long slotStart = Math.max(startTime, timeSlot.getStart());
+            if (slotStart + remainingDuration <= timeSlot.getEnd()) {
+                endTime = slotStart + remainingDuration;
+                remainingDuration = 0;
+                break;
+            } else {
+                remainingDuration -= timeSlot.getEnd() - slotStart;
+            }
         }
+        return endTime;
     }
 }
